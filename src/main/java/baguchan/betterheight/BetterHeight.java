609f9770f225398086c82169bda2c1f510725576
baguchan <shurf012@gmail.com>
2021-10-10 21:53:37 +0900

    fix server crash

--- src/main/java/baguchan/betterheight/BetterHeight.java
+++ src/main/java/baguchan/betterheight/BetterHeight.java
@@ -1,16 +1,10 @@
 package baguchan.betterheight;
 
 import com.google.common.collect.Maps;
-import net.minecraft.client.gui.screens.worldselection.WorldPreset;
-import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.OverworldBiomeSource;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.*;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.StructureFeatureConfiguration;
@@ -35,14 +29,6 @@ public class BetterHeight {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger(MODID);
 
-    public static final WorldPreset NORMAL = new WorldPreset(new TranslatableComponent("betterheight.test")) {
-        protected ChunkGenerator generator(Registry<Biome> p_101580_, Registry<NoiseGeneratorSettings> p_101581_, long p_101582_) {
-            return new NoiseBasedChunkGenerator(new OverworldBiomeSource(p_101582_, false, false, p_101580_), p_101582_, () -> {
-                return p_101581_.getOrThrow(NoiseGeneratorSettings.OVERWORLD);
-            });
-        }
-    };
-
     public BetterHeight() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -57,9 +43,7 @@ public class BetterHeight {
 
     private void setup(final FMLCommonSetupEvent event) {
         ModConfiguredFeatures.init();
-
-        NoiseGeneratorSettings.BUILTIN_OVERWORLD = NoiseGeneratorSettings.register(NoiseGeneratorSettings.OVERWORLD, newOverworld(new StructureSettings(true), false));
-
+        register(NoiseGeneratorSettings.OVERWORLD, newOverworld(new StructureSettings(true), false));
         register(NoiseGeneratorSettings.NETHER, netherLikePreset(new StructureSettings(false), Blocks.NETHERRACK.defaultBlockState(), Blocks.LAVA.defaultBlockState()));
         register(NoiseGeneratorSettings.CAVES, netherLikePreset(new StructureSettings(true), Blocks.STONE.defaultBlockState(), Blocks.WATER.defaultBlockState()));
     }
@@ -92,6 +76,7 @@ public class BetterHeight {
     {
 
     }
+
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
