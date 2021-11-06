f1875a7925e751d6efbe3e2f6eb618aeea292c42
baguchan <shurf012@gmail.com>
2021-11-06 18:19:09 +0900

    height changed and overworld ore gen changed

--- src/main/java/baguchan/betterheight/CaveEventHandler.java
+++ src/main/java/baguchan/betterheight/CaveEventHandler.java
@@ -1,6 +1,7 @@
 package baguchan.betterheight;
 
 import net.minecraft.core.Registry;
+import net.minecraft.data.worldgen.Features;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep;
@@ -22,6 +23,64 @@ public class CaveEventHandler {
 
 			if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.NETHER)) {
 				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_DECORATION, ModConfiguredFeatures.ORE_DEBRIS_UNDER);
+			}
+
+			if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.OVERWORLD)) {
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_COAL_UPPER);
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_COAL_LOWER);
+
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_IRON_UPPER);
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_IRON_MIDDLE);
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_IRON_SMALL);
+
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_GOLD);
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_EMERALD);
+
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_LAPIS);
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_LAPIS_BURIED);
+
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_REDSTONE);
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_REDSTONE_LOWER);
+
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_COPPER);
+
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_DIAMOND);
+				event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Features.PROTOTYPE_ORE_DIAMOND_LARGE);
+			}
+		}
+	}
+
+	@SubscribeEvent(priority = EventPriority.LOW)
+	public static void caveRemoveEventHandler(BiomeLoadingEvent event) {
+		BiomeGenerationSettingsBuilder generation = event.getGeneration();
+		if (event.getName() != null) {
+			ResourceKey<Biome> biome = ResourceKey.create(Registry.BIOME_REGISTRY, event.getName());
+
+			if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.OVERWORLD)) {
+				event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).removeIf(featureSupplier -> {
+					return featureSupplier.get() == Features.ORE_COAL;
+				});
+				event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).removeIf(featureSupplier -> {
+					return featureSupplier.get() == Features.ORE_IRON;
+				});
+				event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).removeIf(featureSupplier -> {
+					return featureSupplier.get() == Features.ORE_GOLD;
+				});
+				event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).removeIf(featureSupplier -> {
+					return featureSupplier.get() == Features.ORE_EMERALD;
+				});
+				event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).removeIf(featureSupplier -> {
+					return featureSupplier.get() == Features.ORE_LAPIS;
+				});
+				event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).removeIf(featureSupplier -> {
+					return featureSupplier.get() == Features.ORE_REDSTONE;
+				});
+				event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).removeIf(featureSupplier -> {
+					return featureSupplier.get() == Features.ORE_COPPER;
+				});
+				event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).removeIf(featureSupplier -> {
+					return featureSupplier.get() == Features.ORE_DIAMOND;
+				});
 			}
 
 		}
