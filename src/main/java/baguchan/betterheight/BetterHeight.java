120f5ba79252c6172a3e89437f655d29b87044a2
baguchan <shurf012@gmail.com>
2021-12-04 18:12:30 +0900

    1.18

--- src/main/java/baguchan/betterheight/BetterHeight.java
+++ src/main/java/baguchan/betterheight/BetterHeight.java
@@ -2,6 +2,8 @@ package baguchan.betterheight;
 
 import com.google.common.collect.Maps;
 import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.data.worldgen.SurfaceRuleData;
+import net.minecraft.data.worldgen.TerrainProvider;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -9,13 +11,11 @@ import net.minecraft.world.level.levelgen.*;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.StructureFeatureConfiguration;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -43,22 +43,13 @@ public class BetterHeight {
 
     private void setup(final FMLCommonSetupEvent event) {
         ModConfiguredFeatures.init();
-        register(NoiseGeneratorSettings.OVERWORLD, newOverworld(new StructureSettings(true), false));
         register(NoiseGeneratorSettings.NETHER, netherLikePreset(new StructureSettings(false), Blocks.NETHERRACK.defaultBlockState(), Blocks.LAVA.defaultBlockState()));
-        register(NoiseGeneratorSettings.CAVES, netherLikePreset(new StructureSettings(true), Blocks.STONE.defaultBlockState(), Blocks.WATER.defaultBlockState()));
-    }
-
-    public static NoiseGeneratorSettings newOverworld(StructureSettings p_158564_, boolean p_158565_) {
-        double d0 = 0.9999999814507745D;
-        Map<StructureFeature<?>, StructureFeatureConfiguration> map = Maps.newHashMap(NoiseGeneratorSettings.bootstrap().structureSettings().structureConfig());
-
-        return new NoiseGeneratorSettings(new StructureSettings(Optional.ofNullable(p_158564_.stronghold()), map), NoiseSettings.create(-64, 384, new NoiseSamplingSettings(0.9999999814507745D, 0.9999999814507745D, 80.0D, 160.0D), new NoiseSlideSettings(-10, 3, 0), new NoiseSlideSettings(15, 3, 0), 1, 2, 1.0D, -0.46875D, true, true, false, p_158565_), Blocks.STONE.defaultBlockState(), Blocks.WATER.defaultBlockState(), Integer.MIN_VALUE, 0, 63, 0, false, true, true, true, true, true);
     }
 
     public static NoiseGeneratorSettings netherLikePreset(StructureSettings p_158554_, BlockState p_158555_, BlockState p_158556_) {
         Map<StructureFeature<?>, StructureFeatureConfiguration> map = Maps.newHashMap(NoiseGeneratorSettings.bootstrap().structureSettings().structureConfig());
         map.put(StructureFeature.RUINED_PORTAL, new StructureFeatureConfiguration(25, 10, 34222645));
-        return new NoiseGeneratorSettings(new StructureSettings(Optional.ofNullable(p_158554_.stronghold()), map), NoiseSettings.create(-64, 256, new NoiseSamplingSettings(1.0D, 3.0D, 80.0D, 60.0D), new NoiseSlideSettings(120, 3, 0), new NoiseSlideSettings(320, 8, -1), 1, 2, 0.0D, 0.019921875D, false, false, false, false), p_158555_, p_158556_, 0, 0, 32, 0, false, false, false, false, false, false);
+        return new NoiseGeneratorSettings(new StructureSettings(Optional.empty(), map), NoiseSettings.create(-64, 256, new NoiseSamplingSettings(1.0D, 3.0D, 80.0D, 60.0D), new NoiseSlider(0.9375D, 3, 0), new NoiseSlider(2.5D, 8, -1), 1, 2, false, false, false, TerrainProvider.nether()), Blocks.NETHERRACK.defaultBlockState(), Blocks.LAVA.defaultBlockState(), SurfaceRuleData.nether(), 32, false, false, false, false, false, true);
     }
 
 
@@ -74,12 +65,6 @@ public class BetterHeight {
 
     private void processIMC(final InterModProcessEvent event)
     {
-
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
 
     }
 }
--- src/main/java/baguchan/betterheight/ModConfiguredFeatures.java
+++ src/main/java/baguchan/betterheight/ModConfiguredFeatures.java
@@ -1,22 +1,17 @@
 package baguchan.betterheight;
 
-import net.minecraft.core.Registry;
-import net.minecraft.data.BuiltinRegistries;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.placement.BiomeFilter;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class ModConfiguredFeatures {
-	public static final ConfiguredFeature<?, ?> ORE_DEBRIS_UNDER = register(BetterHeight.MODID + ":ore_debris_under", Feature.SCATTERED_ORE.configured(new OreConfiguration(OreConfiguration.Predicates.NETHER_ORE_REPLACEABLES, Blocks.ANCIENT_DEBRIS.defaultBlockState(), 6, 1.0F)).rangeTriangle(VerticalAnchor.bottom(), VerticalAnchor.absolute(0)).squared());
+	public static final PlacedFeature ORE_ANCIENT_DEBRIS_UNDER_LARGE = PlacementUtils.register("betterheight:ore_ancient_debris_under_large", OreFeatures.ORE_ANCIENT_DEBRIS_LARGE.placed(InSquarePlacement.spread(), HeightRangePlacement.triangle(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(0)), BiomeFilter.biome()));
 
 	public static void init() {
 
-	}
-
-	private static <FC extends FeatureConfiguration> ConfiguredFeature<FC, ?> register(String p_127056_, ConfiguredFeature<FC, ?> p_127057_) {
-		return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, p_127056_, p_127057_);
 	}
 }
